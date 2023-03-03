087be2b2cea0224f3a66a4037e0b4beb90933fd9
onusra <onusra@angelo.edu>
2023-03-03 20:21:13 +0000

    ArrayCombine

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class CombineArrays {
     /**
      * Combine two arrays into a single array and return it.
@@ -7,8 +7,18 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
+    public static void main(String [] args){
 
-        return null;
+    }
+    public int[] combine(int[] arr1, int[] arr2){
+       int arr3[] = new int[(arr1.length ) + (arr2.length)];
+       for(int i = 0; i < arr1.length; i++){
+            arr3[i] = arr1[i];
+       }
+       int size = arr1.length;
+       for(int i = 0; i < arr2.length; i++){
+            arr3[size + i] = arr2[i];
+       }
+        return arr3;
     }
 }
